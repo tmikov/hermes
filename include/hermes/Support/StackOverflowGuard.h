59b2d63cbb5cbb65d3013345aa5ed7cc847ce95a
Tzvetan Mikov <tmikov@meta.com>
2025-11-23 19:59:41 -0800

    fix stack checking

--- include/hermes/Support/StackOverflowGuard.h
+++ include/hermes/Support/StackOverflowGuard.h
@@ -27,6 +27,10 @@
 #ifndef HERMES_SUPPORT_STACKOVERFLOWGUARD_H
 #define HERMES_SUPPORT_STACKOVERFLOWGUARD_H
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
 #include "llvh/Support/Compiler.h"
 
 #include <cstddef>
@@ -65,8 +69,13 @@ class StackOverflowGuard {
     // We know that nativeStackSize_ <= nativeStackHigh_
     // (because otherwise the stack wouldn't fit in the memory),
     // so the overflowed difference will be greater than nativeStackSize_.
+#ifdef _MSC_VER
+    void *stackPointer = _AddressOfReturnAddress();
+#else
+    void *stackPointer = __builtin_frame_address(0);
+#endif
     if (LLVM_LIKELY(!(
-            (uintptr_t)nativeStackHigh - (uintptr_t)__builtin_frame_address(0) >
+            (uintptr_t)nativeStackHigh - (uintptr_t)stackPointer >
             nativeStackSize))) {
       // Fast path: quickly check the stored stack bounds.
       // NOTE: It is possible to have a false negative here (highly unlikely).
